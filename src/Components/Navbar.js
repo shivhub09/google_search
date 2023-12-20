41278ccf7bdf19a0a8e7e41924f2c1e366af9f8c
shivhub09 <shivnagori2020@gmail.com>
2023-12-20 21:15:48 +0530

    navbar modified

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import {Link } from 'react-router-dom';
-import {Router} from './Router';
-import {Footer} from './Footer';
-import Search from './Search';
+import { Link } from 'react-router-dom';
 
-export const Navbar = () => {
-  return (
-    <div className='p-5 pb-5 flex flex-wrap sm:justify-between justify-center border-b border-gray-500 '>
-        <div className='flex flex-justify-between items-center space-x-5  w-screen pt-4'>
-            <Link to="/">
-                <p className=' text-3xl bg-blue-200 font-bold text-gray-700 py-2 px-3 rounded'>
-                  Google🔎
-                </p>
-            </Link>
-            <Search/>
-        </div>
+import  Search from './Search';
+
+export const Navbar = ({ setDarkTheme, darkTheme }) => (
+  <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
+    <div className="flex justify-between items-center space-x-5 w-screen ">
+      <Link to="/">
+        <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
+          Goggl 🔎
+        </p>
+      </Link>
+      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡 Light' : '🌙 Dark'}</button>
     </div>
-  )
-}
-
+    <Search />
+  </div>
+);
